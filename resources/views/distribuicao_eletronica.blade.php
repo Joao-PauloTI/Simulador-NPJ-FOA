2711d6c57ba9b05e04e8bcd70501817a989e147d
Joao Paulo <joaopaulo_ti@hotmail.com>
2019-05-12 22:11:53 -0300

    continuação da página de distribuição_eletronica

--- resources/views/distribuicao_eletronica.blade.php
+++ resources/views/distribuicao_eletronica.blade.php
@@ -18,24 +18,38 @@
             <!--Isto é apenas uma amostra dos casos. No programa real, nenhuma das opções devem estar 
                 selecionadas quando a página carregar! O certo é aparecer um caso diferente de acordo 
                 com a opção selecionada.-->
-            <!--if (grerjSim == true)-->
-                <label class="radio-inline"><input type="radio" name="grerjSim" checked> Sim </label>
-                <label class="radio-inline"><input type="radio" name="grerjNao"> Não </label>
+
+                <!--Se for selecionado sim-->
+                <div class="custom-control custom-control-inline custom-radio">
+                    <input type="radio" class="custom-control-input" name="grerjSim" id="grerjSim" checked>
+                    <label class="custom-control-label" for="grerjSim"> Sim </label>
+                </div>
+                <div class="custom-control custom-control-inline custom-radio">
+                    <input type="radio" class="custom-control-input" name="grerjNao" id="grerjNao">
+                    <label class="custom-control-label" for="grerjNao"> Não </label>
+                </div>
                 <br>
                 <span><strong>*</strong> Número da GRERJ </span>
                 <input type="text" required>
                 <br>
-            <!--else
-                <label class="radio-inline"><input type="radio" name="grerjSim"> Sim </label>
-                <label class="radio-inline"><input type="radio" name="grerjNao" checked> Não </label>
+            <!--Se for selecionado não
+                <div class="custom-control custom-control-inline custom-radio">
+                    <input type="radio" class="custom-control-input" name="grerjSim" id="grerjSim">
+                    <label class="custom-control-label" for="grerjSim"> Sim </label>
+                </div>
+                <div class="custom-control custom-control-inline custom-radio">
+                    <input type="radio" class="custom-control-input" name="grerjNao" id="grerjNao" checked>
+                    <label class="custom-control-label" for="grerjNao"> Não </label>
+                </div>
                 <br>
-                <span><strong>*</strong> Motivo</span>
-                <select>
-                    <option>Escolha um motivo</option>
-                    <option>Juizado Especial Cível</option>
-                    <option>Justiça Gratuita / Pedido de Gratuidade</option>
-                </select>
-            endif-->
+                <label for="grerjMotivo">Motivo 
+                    <select id="grerjMotivo" class="custom-select">
+                        <option>Escolha um motivo</option>
+                        <option>Juizado Especial Cível</option>
+                        <option>Justiça Gratuita / Pedido de Gratuidade</option>
+                    </select>
+                </label>
+            -->
         </div>
     </div>
     <br>
@@ -46,8 +60,14 @@
         </div>
         <div class="card-body">
             <span><strong>*</strong> Distribuição por Dependência </span>
-            <label class="radio-inline"><input type="radio" name="dpdSim"> Sim</label>
-            <label class="radio-inline"><input type="radio" name="dpdNao" checked> Não</label>
+            <div class="custom-control custom-control-inline custom-radio">
+                <input type="radio" class="custom-control-input" name="dpdSim" id="dpdSim">
+                <label class="custom-control-label" for="dpdSim"> Sim </label>
+            </div>
+            <div class="custom-control custom-control-inline custom-radio">
+                <input type="radio" class="custom-control-input" name="dpdNao" id="dpdNao">
+                <label class="custom-control-label" for="dpdNao"> Não </label>
+            </div>
             <br>
         </div>
     </div>
@@ -60,36 +80,41 @@
         <!--É preciso perguntar as opções de marcar abaixo são necessárias-->
         <div class="card-body">
             <form action="">
-                <div class="form-check-inline">
-                    <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" name="ddp1"> Prioridade p/ deficiente físico ou mental 
+                <div class="custom-control custom-control-inline custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" name="ddp1" id="ddp1">
+                    <label class="custom-control-label" for="ddp1">
+                         Prioridade p/ deficiente físico ou mental 
                     </label>
                 </div>
-                <div class="form-check-inline">
-                    <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" name="ddp2"> Tutela de Urgência 
+                <div class="custom-control custom-control-inline custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" name="ddp2" id="ddp2">
+                    <label class="custom-control-label" for="ddp2">
+                         Tutela de Urgência 
                     </label>
                 </div>
-                <div class="form-check-inline">
-                    <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" name="ddp3"> Prioridade p/ portador de doença grave 
+                <div class="custom-control custom-control-inline custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" name="ddp3" id="ddp3">
+                    <label class="custom-control-label" for="ddp3">
+                         Prioridade p/ portador de doença grave 
                     </label>
                 </div>
-                <div class="form-check-inline">
-                    <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" name="ddp4"> Gratuidade de Justiça 
+                <div class="custom-control custom-control-inline custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" name="ddp4" id="ddp4">
+                    <label class="custom-control-label" for="ddp4">
+                         Gratuidade de Justiça 
                     </label>
                 </div>
-                <div class="form-check-inline">
-                    <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input" name="ddp5"> Exame Técnico 
+                <div class="custom-control custom-control-inline custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" name="ddp5" id="ddp5">
+                    <label class="custom-control-label" for="ddp5">
+                         Exame Técnico 
                     </label>
                 </div>
             </form>
             <br>
             <!--Todos os campos são interligados-->
             <label for="ddp_comarca"><strong>*</strong> Comarca
-                <select id="ddp_comarca">
+                <select class="custom-select"  id="ddp_comarca">
                     <option>Escolha uma comarca</option>
                     <option>Comarca de Barra do Piraí</option>
                     <option>Comarca de Volta Redonda</option>
@@ -98,7 +123,7 @@
             </label>
             <br>
             <label for="ddp_competencia"><strong>*</strong> Competência 
-                <select id="ddp_competencia">
+                <select class="custom-select" id="ddp_competencia">
                     <option>Escolha uma competência</option>
                     <option>Acidentes de Trabalho</option>
                     <option>Juizado Especial Cível</option>
@@ -107,7 +132,7 @@
             </label>
             <br>
             <label for="ddp_classe"><strong>*</strong> Classe 
-                <select id="ddp_classe">
+                <select class="custom-select" id="ddp_classe">
                     <option>Escolha uma classe</option>
                     <option>Ação Civil Coletiva</option>
                     <option>Alvará Judicial</option>
@@ -116,7 +141,7 @@
             </label>
             <br>
             <label for="ddp_distribuicao"><strong>*</strong> Distribuição 
-                <select id="ddp_distribuicao">
+                <select class="custom-select" id="ddp_distribuicao">
                     <option>Escolha uma distribuição</option>
                     <option>...</option>
                     <option>...</option>
@@ -129,5 +154,96 @@
             <br>
         </div>
     </div>
+    <br>
+    <!--Advogado/Representante-->
+    <div class="card">
+        <div class="card-header">
+            <h5><strong>Advogado / Representante</strong></h5>
+        </div>
+        <div class="card-body">
+            <select>
+                <option>Advogado</option>
+                <option>Defensor Público</option>
+            </select>
+            <!--if(advogado == true)-->
+            <span> Número de OAB </span>
+            <select>
+                <option>RJ</option>
+                <option>SP</option>
+                <option>...</option>
+            </select>
+            <input type="text" required>
+            <!--else
+            <span> Matrícula </span>
+            <input type="text" required>
+            endif-->
+        </div>
+    </div>
+    <br>
+    <!--Autores-->
+    <div class="card">
+        <div class="card-header">
+            <h5><strong>Autor(es)</strong></h5>
+        </div>
+        <div class="card-body">
+            <a href="#">Cadastrar Autor </a>
+            <span> | </span>
+            <a href="#"> Cadastrar Representante do Autor </a>
+        </div>
+    </div>
+    <br>
+    <!--Réus-->
+    <div class="card">
+        <div class="card-header">
+            <h5><strong>Réu(s)</strong></h5>
+        </div>
+        <div class="card-body">
+            <a href="#">Cadastrar Réu </a>
+            <span> | </span>
+            <a href="#"> Cadastrar Representante do Réu </a>
+        </div>
+    </div>
+    <br>
+    <!--Documentos-->
+    <div class="card">
+        <div class="card-header">
+            <h5><strong>Documento(s)</strong></h5>
+        </div>
+        <div class="card-body">
+            <a href="#">Upload da Petição Inicial </a>
+            <span> | </span>
+            <a href="#"> Enviar CPF </a>
+            <span> | </span>
+            <a href="#"> Enviar Comprovante de Residência </a>
+            <span> | </span>
+            <a href="#"> Enviar Procuração </a>
+            <span> | </span>
+            <a href="#"> Upload do(s) Anexo(s) da Petição Inicial </a>
+        </div>
+    </div>
+    <br>
+    <!--Declaração de Veracidade-->
+    <div class="card">
+        <div class="card-header">
+            <h5><strong>Declaração de Veracidade</strong></h5>
+        </div>
+        <div class="card-body">
+            <form action="">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="ddvCheck">
+                    <label class="custom-control-label" for="ddvCheck"> "Declaro sob as penas da Lei, que as informações acima prestadas correspondem à verdade, tendo sido prestadas em obediência à lealdade processual e à boa fé, nos termos do Art. 5º do CPC/15, ciente de que a eventual prestação de informações inverídicas poderá acarretar a incidência das penalidades previstas em lei."</label>
+                </div>
+            </form>
+        </div>
+    </div>
+    <br>
+    <!--Botões SALVAR/LIMPAR-->
+    <div style="text-align:center">
+        <form action="">
+            <button type="submit" class="btn btn-primary">SALVAR</button>
+            <button type="reset" class="btn btn-danger">LIMPAR</button>
+        </form>
+    </div>
+    <br>
 </div>
 @endsection
