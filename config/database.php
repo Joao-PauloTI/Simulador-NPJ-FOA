98b7109d53a240fd2405307503c1502ae6620d25
João Paulo <joaopaulo_ti@hotmail.com>
2019-11-04 13:13:00 -0300

    Teste com mysql no heroku 2

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'DB_Simulador'),
             'username' => env('DB_USERNAME', 'joao-paulo'),
