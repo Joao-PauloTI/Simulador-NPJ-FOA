4249f3d640e01d9688d11019066bc081f1ec9de5
João Paulo <joaopaulo_ti@hotmail.com>
2019-11-04 13:19:25 -0300

    teste com mysql no heroku 3

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'DB_Simulador'),
-            'username' => env('DB_USERNAME', 'joao-paulo'),
-            'password' => env('DB_PASSWORD', '23021999'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
